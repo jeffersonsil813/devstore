e9cbb0b9d77483d466984679723e6d9a73fb3aa6
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-07-21 19:26:37 -0300

    initial config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from "@eslint/compat";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...compat.extends("@rocketseat/eslint-config/next"),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1 className="text-2xl text-red-400">Hello World!</h1>
+  );
+};
+
+export default Home;
