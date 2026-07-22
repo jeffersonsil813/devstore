b2f9ccf37bd86b838a5f80a7a072543cfaf81dd9
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-07-22 15:39:03 -0300

    create the header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-50 antialiased">
+        {children}
+      </body>
     </html>
   );
 }
