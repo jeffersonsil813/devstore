2633470f7cb0d2ae43f12f11f9a05b868084b0a8
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-08-14 14:37:57 -0300

    fix build errors caused by missing suspense boundary

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,18 +1,9 @@
-"use client";
-
 import Skeleton from "@/components/skeleton";
-import { useSearchParams } from "next/navigation";
 
 const SearchLoading = () => {
-  const searchParams = useSearchParams();
-
-  const query = searchParams.get("q");
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <p className="text-sm">Buscando produtos...</p>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-95" />
